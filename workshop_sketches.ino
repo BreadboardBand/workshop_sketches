a3d39109e0ac6bd0b6c22a507283707e08cbcf22
kaz saita <saita@kinoshita-lab.org>
2012-07-28 15:41:09 +0900

    DIPスイッチが抜けているときにはオシレーターになるように変更しました。

--- workshop_sketches.ino
+++ workshop_sketches.ino
@@ -8,9 +8,8 @@ int seqpin1 = 1; //可変抵抗2ステップ目をつなぐピン
 int seqpin2 = 2; //可変抵抗3ステップ目をつなぐピン 
 int seqpin3 = 3; //可変抵抗4ステップ目をつなぐピン 
 int sensorpin = 5;
-int tempopin = 4; //テンポを変更 
+int tempopin = 4; //テンポを変更
 
-// saita modified
 static const int NUMBER_OF_SWITCHES = 2;
 static const int switchPorts[NUMBER_OF_SWITCHES] = {2, 3};
 int lastModeSwitchValues[NUMBER_OF_SWITCHES] = { -1, -1 };
@@ -22,8 +21,8 @@ float noteval;  //周波数
 
 enum Mode
 {
-  OSCILLATOR = 0,
-  SEQUENCER,
+  SEQUENCER = 0,
+  OSCILLATOR,
   NUMBER_OF_MODES,
 };
 
@@ -57,9 +56,9 @@ void setup()
   Serial.begin(9600);
   
   for(int i = 0; i < NUMBER_OF_SWITCHES; ++i) { 
-    // make them input
+    // DIPスイッチの設定（入力）
     pinMode(switchPorts[i], INPUT);
-    // make them pullup
+    // DIPスイッチの設定(何もつながれていない時にはHIGHにする)
     digitalWrite(switchPorts[i], HIGH);
   }
 }
@@ -133,3 +132,28 @@ void seq(){
 
 }
 
+/*
+Copyright (c) 2011 - 2012 , Breadboard Band
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+- Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+- Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+- Neither the name of the Breadboard Band nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/*
+Copyright (c) 2011 - 2012, Breadboard Band
+All rights reserved.
+
+ソースコード形式かバイナリ形式か、変更するかしないかを問わず、以下の条件を満たす場合に限り、再頒布および使用が許可されます。
+
+- ソースコードを再頒布する場合、上記の著作権表示、本条件一覧、および下記免責条項を含めること。
+- バイナリ形式で再頒布する場合、頒布物に付属のドキュメント等の資料に、上記の著作権表示、本条件一覧、および下記免責条項を含めること。
+- 書面による特別の許可なしに、本ソフトウェアから派生した製品の宣伝または販売促進に、Breadboard Bandの名前またはコントリビューターの名前を使用してはならない。
+
+本ソフトウェアは、著作権者およびコントリビューターによって「現状のまま」提供されており、明示黙示を問わず、商業的な使用可能性、および特定の目的に対する適合性に関する暗黙の保証も含め、またそれに限定されない、いかなる保証もありません。著作権者もコントリビューターも、事由のいかんを問わず、 損害発生の原因いかんを問わず、かつ責任の根拠が契約であるか厳格責任であるか（過失その他の）不法行為であるかを問わず、仮にそのような損害が発生する可能性を知らされていたとしても、本ソフトウェアの使用によって発生した（代替品または代用サービスの調達、使用の喪失、データの喪失、利益の喪失、業務の中断も含め、またそれに限定されない）直接損害、間接損害、偶発的な損害、特別損害、懲罰的損害、または結果損害について、一切責任を負わないものとします。
+*/
